63803fc08a4fbe7e33cc9e38016b33e4d7eed78d
Steven Nguyen <qviet715@gmail.com>
2023-01-14 22:24:36 -0500

    create generatePassword function

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+var numbers = "0123456789";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var symbols = "~!@#$%^&*()";
 
 
 // Get references to the #generate element
@@ -15,3 +19,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var all = "";
+
+  // All possible character variables availables for user-prompted input
+  // Create a variable named "length" and assign value for user-prompted input, convert to integer using parseInt().
+  var length = parseInt(window.prompt("Pick a password length from 8 -128.")); 
+
+  // If user-prompted input is less that 
+  if(length <8 || length >128) {
+    alert("Password length is not between 8 - 128");
+    location.reload();
+  }
+}
