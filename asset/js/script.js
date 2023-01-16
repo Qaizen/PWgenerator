c550e1cb6afb48a098dca750b107fa75bf975c3e
Steven Nguyen <qviet715@gmail.com>
2023-01-16 15:40:10 -0500

    fixed if statement for pw length to create error

--- asset/js/script.js
+++ asset/js/script.js
@@ -4,7 +4,6 @@ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var symbols = "~!@#$%^&*()";
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -20,45 +19,53 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var all = "";
+  var all = "";  // All possible character variables availables for input
 
-  // All possible character variables availables for input
   // Create a variable named "length" and assign value for input, convert to integer using parseInt().
-  var length = parseInt(window.prompt("Pick a password length from 8 -128.")); 
+  var length = parseInt(window.prompt("Pick a numercial password length from 8-128."));
 
-  // If input is less 8 or more than 128 show message and try again
-  if(length <8 || length >128) {
-    alert("Please enter a password length between 8 - 128 characters.");
-    location.reload();
-  }
+  // If input is less 8 or more than 128 show an error message prompt user to enter proper numercial value
+  // if an error is produced again reset and return "undefined"
+  if (length < 8 || length > 128) {
+    alert("Error: Please enter a password length between 8 - 128 characters.");
+    var input = window.prompt("Please enter a numerical password length between 8-128 characters: ");
+    length = parseInt(input);
+    if (length < 8 || length > 128) {
+        return alert("Error: Invalid input, please try again");
+    }
+}
 
   // Confirm whether or not to use lowercase
-  if (window.confirm("Do you want your password to contain lowercase letters?")) {
+  if (
+    window.confirm("Do you want your password to contain lowercase letters?")
+  ) {
     all += lower; // all = all + lower
   }
-  
+
   // Confirm whether or not to use uppercase
-  if (window.confirm("Do you want your password to contain uppercase letters?")) {
+  if (
+    window.confirm("Do you want your password to contain uppercase letters?")
+  ) {
     all += upper;
   }
 
   // Confirm whether or not to use numercial values
-  if(window.confirm("Do you want numerical values for your password?")){
+  if (window.confirm("Do you want your password to contain numerical values?")) {
     all += numbers;
   }
 
-  // Confirm whether or not to use numerical values
-  if (window.confirm("Do you want your password to contain special characters?")) {
+  // Confirm whether or not to use special characters
+  if (
+    window.confirm("Do you want your password to contain special characters?")
+  ) {
     all += symbols;
   }
-  
-// Create a variable called "allpw" and initialize as an empty string. Then it enters a for loop that will run for a number of iterations equal to the value of the "length" variable. On each iteration of the loop, a random character is chosen from the "all" string and added to the "allpw" variable using the += operator. The random character is chosen by using the Math.random() method, which returns a random number between 0 and 1, and multiplying it by the length of the "all" string. The Math.floor() method is used to round the result down to the nearest whole number, so that the result can be used as an index to retrieve a character from the "all" string. The for loop constructs a string of random characters of the length specified by the "length" variable. 
 
-var allpw = "";
+  // Create a variable called "allpw" and initialize as an empty string. Then it enters a for loop that will run for a number of iterations equal to the value of the "length" variable. On each iteration of the loop, a random character is chosen from the "all" string and added to the "allpw" variable using the += operator. The random character is chosen by using the Math.random() method, which returns a random number between 0 and 1, and multiplying it by the length of the "all" string. The Math.floor() method is used to round the result down to the nearest whole number, so that the result can be used as an index to retrieve a character from the "all" string. The for loop constructs a string of random characters of the length specified by the "length" variable.
+
+  var allpw = "";
   for (var i = 0; i < length; i++) {
     allpw += all.charAt(Math.floor(Math.random() * all.length));
-
+  }
+  return allpw; // return the generated password here
 }
-return allpw;   // return the generated password here
-}
-
