802dd12ad556d2ef4191a5e1621284737db551a9
Steven Nguyen <qviet715@gmail.com>
2023-01-16 14:29:21 -0500

    linked css and js files

--- asset/js/script.js
+++ asset/js/script.js
@@ -34,7 +34,7 @@ function generatePassword() {
 
   // Confirm whether or not to use lowercase
   if (window.confirm("Do you want your password to contain lowercase letters?")) {
-    all += lower;
+    all += lower; // all = all + lower
   }
   
   // Confirm whether or not to use uppercase
@@ -45,11 +45,20 @@ function generatePassword() {
   // Confirm whether or not to use numercial values
   if(window.confirm("Do you want numerical values for your password?")){
     all += numbers;
+  }
 
   // Confirm whether or not to use numerical values
   if (window.confirm("Do you want your password to contain special characters?")) {
     all += symbols;
   }
   
+// Create a variable called "allpw" and initialize as an empty string. Then it enters a for loop that will run for a number of iterations equal to the value of the "length" variable. On each iteration of the loop, a random character is chosen from the "all" string and added to the "allpw" variable using the += operator. The random character is chosen by using the Math.random() method, which returns a random number between 0 and 1, and multiplying it by the length of the "all" string. The Math.floor() method is used to round the result down to the nearest whole number, so that the result can be used as an index to retrieve a character from the "all" string. The for loop constructs a string of random characters of the length specified by the "length" variable. 
+
+var allpw = "";
+  for (var i = 0; i < length; i++) {
+    allpw += all.charAt(Math.floor(Math.random() * all.length));
+
 }
+return allpw;   // return the generated password here
 }
+
